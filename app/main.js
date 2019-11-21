e92f63200903daf6b617c76767e53fe84aa27678
BlueRaptor28 <graysonbuc@gmail.com>
2019-11-21 15:21:32 -0500

    drag and drop for new blocks

--- app/main.js
+++ app/main.js
@@ -1,7 +1,11 @@
 const fill = document.querySelector('.fill');
 const step1 = document.querySelectorAll('.step1');
 const step2 = document.querySelectorAll('.step2');
+const step22 = document.querySelectorAll('.step22');
+const step23 = document.querySelectorAll('.step23');
+const step24 = document.querySelectorAll('.step24');
 const step3 = document.querySelectorAll('.step3');
+const step32 = document.querySelectorAll('.step3');
 const step4 = document.querySelectorAll('.step4');
 const step5 = document.querySelectorAll('.step5');
 
@@ -22,7 +26,31 @@ for(const empty of step2) {
     empty.addEventListener('dragleave', dragLeave);
     empty.addEventListener('drop', dragDrop);
 }
+for(const empty of step22) {
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
+}
+for(const empty of step23) {
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
+}
+for(const empty of step24) {
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
+}
 for(const empty of step3) {
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
+}
+for(const empty of step32) {
     empty.addEventListener('dragover', dragOver);
     empty.addEventListener('dragenter', dragEnter);
     empty.addEventListener('dragleave', dragLeave);
@@ -68,8 +96,20 @@ function dragLeave() {
     else if(this.className == 'step2') {
         this.className = 'step2';
     }
+    else if(this.className == 'step22') {
+        this.className = 'step22';
+    }
+    else if(this.className == 'step23') {
+        this.className = 'step23';
+    }
+    else if(this.className == 'step24') {
+        this.className = 'step24';
+    }
     else if(this.className == 'step3') {
         this.className = 'step3';
+    }
+    else if(this.className == 'step3') {
+        this.className = 'step32';
     }
     else if(this.className == 'step4') {
         this.className = 'step4';
@@ -86,8 +126,20 @@ function dragDrop() {
     else if(this.className == 'step2') {
         this.className = 'step2';
     }
+    else if(this.className == 'step22') {
+        this.className = 'step22';
+    }
+    else if(this.className == 'step23') {
+        this.className = 'step23';
+    }
+    else if(this.className == 'step24') {
+        this.className = 'step24';
+    }
     else if(this.className == 'step3') {
         this.className = 'step3';
+    }
+    else if(this.className == 'step3') {
+        this.className = 'step32';
     }
     else if(this.className == 'step4') {
         this.className = 'step4';
