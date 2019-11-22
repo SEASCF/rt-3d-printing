f814035fdb263ceea28cfcc6a2efae97ef39ce4a
SEAS CF <diomedes@SEAS13916.cable.rcn.com>
2019-11-22 15:05:53 -0500

    i

--- app/main.js
+++ app/main.js
@@ -1,6 +1,10 @@
 const fill = document.querySelector('.fill');
 const step1 = document.querySelectorAll('.step1');
+const step22 = document.querySelectorAll('.step22');
+const step23 = document.querySelectorAll('.step23');
+const step24 = document.querySelectorAll('.step24');
 const step2 = document.querySelectorAll('.step2');
+const step31 = document.querySelectorAll('.step31');
 const step3 = document.querySelectorAll('.step3');
 const step4 = document.querySelectorAll('.step4');
 const step5 = document.querySelectorAll('.step5');
@@ -16,7 +20,31 @@ for(const empty of step1) {
     empty.addEventListener('dragleave', dragLeave);
     empty.addEventListener('drop', dragDrop);
 }
+for(const empty of step22) {
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
+}
+for(const empty of step23) {
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
+}
+for(const empty of step24) {
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
+}
 for(const empty of step2) {
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
+}
+for(const empty of step31) {
     empty.addEventListener('dragover', dragOver);
     empty.addEventListener('dragenter', dragEnter);
     empty.addEventListener('dragleave', dragLeave);
@@ -65,8 +93,20 @@ function dragLeave() {
     if(this.className == 'step1') {
         this.className = 'step1';
     }
+    else if(this.className == 'step22') {
+        this.className = 'step22';
+    }
+    else if(this.className == 'step23') {
+        this.className = 'step23';
+    }
+    else if(this.className == 'step24') {
+        this.className = 'step24';
+    }
     else if(this.className == 'step2') {
         this.className = 'step2';
+    }
+    else if(this.className == 'step31') {
+        this.className = 'step3';
     }
     else if(this.className == 'step3') {
         this.className = 'step3';
@@ -83,8 +123,20 @@ function dragDrop() {
     if(this.className == 'step1') {
         this.className = 'step1';
     }
+    else if(this.className == 'step22') {
+        this.className = 'step22';
+    }
+    else if(this.className == 'step23') {
+        this.className = 'step23';
+    }
+    else if(this.className == 'step24') {
+        this.className = 'step24';
+    }
     else if(this.className == 'step2') {
         this.className = 'step2';
+    }
+    else if(this.className == 'step31') {
+        this.className = 'step31';
     }
     else if(this.className == 'step3') {
         this.className = 'step3';
