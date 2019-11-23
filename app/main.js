5b68afff0340aa41d2eb982bfbc9bb0c2ebb57f2
SEAS CF <diomedes@SEAS13916.local>
2019-11-23 13:33:45 -0500

    hi

--- app/main.js
+++ app/main.js
@@ -4,9 +4,6 @@ const step22 = document.querySelectorAll('.step22');
 const step23 = document.querySelectorAll('.step23');
 const step24 = document.querySelectorAll('.step24');
 const step2 = document.querySelectorAll('.step2');
-const step22 = document.querySelectorAll('.step22');
-const step23 = document.querySelectorAll('.step23');
-const step24 = document.querySelectorAll('.step24');
 const step3 = document.querySelectorAll('.step3');
 const step32 = document.querySelectorAll('.step32');
 const step4 = document.querySelectorAll('.step4');
@@ -42,25 +39,6 @@ for(const empty of step24) {
     empty.addEventListener('drop', dragDrop);
 }
 for(const empty of step2) {
-    empty.addEventListener('dragover', dragOver);
-    empty.addEventListener('dragenter', dragEnter);
-    empty.addEventListener('dragleave', dragLeave);
-    empty.addEventListener('drop', dragDrop);
-}
-for(const empty of step31) {
-for(const empty of step22) {
-    empty.addEventListener('dragover', dragOver);
-    empty.addEventListener('dragenter', dragEnter);
-    empty.addEventListener('dragleave', dragLeave);
-    empty.addEventListener('drop', dragDrop);
-}
-for(const empty of step23) {
-    empty.addEventListener('dragover', dragOver);
-    empty.addEventListener('dragenter', dragEnter);
-    empty.addEventListener('dragleave', dragLeave);
-    empty.addEventListener('drop', dragDrop);
-}
-for(const empty of step24) {
     empty.addEventListener('dragover', dragOver);
     empty.addEventListener('dragenter', dragEnter);
     empty.addEventListener('dragleave', dragLeave);
@@ -129,6 +107,7 @@ function dragLeave() {
     }
     else if(this.className == 'step31') {
         this.className = 'step3';
+    }
     else if(this.className == 'step22') {
         this.className = 'step22';
     }
@@ -170,6 +149,7 @@ function dragDrop() {
     }
     else if(this.className == 'step31') {
         this.className = 'step31';
+    }
     else if(this.className == 'step22') {
         this.className = 'step22';
     }
