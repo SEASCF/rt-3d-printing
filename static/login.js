def276515d8e4ee392ca0b8bf181dbe35af63418
tuhinadasgupta <tuhinad@gwu.edu>
2020-04-23 15:56:30 +0000

    added basic login modal

---
+++ static/login.js
@@ -0,0 +1,12 @@
+function displayModal(){
+	document.getElementById('id01').style.display='block'; 
+	// Get the modal
+	var modal = document.getElementById('id01');
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+   	 if (event.target == modal) {
+        	modal.style.display = "none";
+   		 }
+	}
+}
