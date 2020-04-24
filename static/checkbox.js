5598dcfd30f8c72fce907af25de46ce9fe8d867d
tuhinadasgupta <tuhinad@gwu.edu>
2020-04-24 16:57:28 +0000

    js files

--- static/checkbox.js
+++ static/checkbox.js
@@ -1,7 +1,28 @@
 function doalert(checkboxElem) {
   if (checkboxElem.checked) {
-alert("checked")
+	alert("checked")
+	document.getElementById("checked").setAttribute("id","color");   
   } else {
-    alert("notchecked")
+       alert("notchecked")
+	  document.getElementById("color").setAttribute("id","checked");
   }
 }
+// Pass the checkbox name to the function
+function getCheckedBoxes(checkboxElem) {
+  var checkboxes = document.getElementsByName(checkboxElem);
+  var checkboxesChecked = [];
+  // loop over them all
+  for (var i=0; i<checkboxes.length; i++) {
+     // And stick the checked ones onto an array...
+     if (checkboxes[i].checked) {
+        checkboxesChecked.push(checkboxes[i]);
+     }
+  }
+  // Return the array if it is non-empty, or null
+  if(checkboxesChecked != null){
+	console.log(checkboxesChecked); 
+  }
+}
+
+// Call as
+//var checkedBoxes = getCheckedBoxes("mycheckboxes");
