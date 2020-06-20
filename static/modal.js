9da85b233f86030314ab1e1208e73a44e480c218
Tuhina Dasgupta <tuhinad@gwu.edu>
2020-06-20 16:07:48 -0700

    add info we already have

--- static/modal.js
+++ static/modal.js
@@ -1,5 +1,10 @@
 function populateModal(theForm){
-    //populate ticketid
+    //populate already had info
     var ticketId = theForm.innerText.substring(0, 5); 
+    var dateCreated = theForm.innerText.substring(8,18); 
+    var requestorName = theForm.innerText.substring(19); 
+
     document.getElementById("modal-ticketid").innerHTML = "ID: " + ticketId; 
+    document.getElementById("modal-ticketdc").innerHTML = "Date Created: " + dateCreated; 
+    document.getElementById("modal-ticketreq").innerHTML = "Requestor: " + requestorName; 
 }
